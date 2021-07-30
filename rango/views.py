c539a85f104f07309ab189ec67ae4523d3f377c7
Zhang Zeyu <2541426z@student.gla.ac.uk>
2021-07-30 09:51:05 +0800

    add categories

--- rango/views.py
+++ rango/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-
+from rango.models import Category
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
 
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
 
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     return render(request, 'rango/about.html')
